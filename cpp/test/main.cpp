963ddcfb45fbe2c448e19c2e3b9a6f63ddfc887e
lize <GW00301491@ifyou.com>
2023-03-28 16:36:20 +0800

    添加无锁队列阻塞模式

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: lize GW00301491@ifyou.com
+ * @Date: 2022-12-06 16:14:40
+ * @LastEditors: lize GW00301491@ifyou.com
+ * @LastEditTime: 2023-02-02 11:24:16
+ * @FilePath: /record/cpp/test/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include <unistd.h>
 
 #include <iostream>
@@ -6,6 +14,7 @@
 #include <thread>
 
 #include "./concurrentqueue.h"
+#include "./blockingconcurrentqueue.h"
 
 using namespace std;
 int num = 0;
@@ -21,7 +30,8 @@ int main() {
       "890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
   // 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
   string data(buf);
-  moodycamel::ConcurrentQueue<string> ccq;
+  // moodycamel::ConcurrentQueue<string> ccq;
+  moodycamel::BlockingConcurrentQueue<string> ccq;
   for (int i = 0; i != 10; ++i) {
     threads[i] = std::thread(
         [&](int i) {
@@ -46,7 +56,7 @@ int main() {
             clock_gettime(CLOCK_REALTIME, &end);
             uint64_t every_dur = end.tv_sec * 1000000000 + end.tv_nsec - (begin.tv_sec * 1000000000 + begin.tv_nsec);
             //   sleep(5);
-            //   printf("生产数据消耗时间max_duration增大为%lums\n", max_duration );
+              printf("生产数据消耗时间max_duration增大为%lums\n", max_duration );
             // if(max_duration / 1000 > 200) printf("大于200ns\n");
             if (every_dur > max_duration) {
               max_duration = every_dur;
@@ -65,7 +75,8 @@ int main() {
       string item;
       int i = 1;
       while (1) {
-        ccq.try_dequeue(item);
+        // ccq.try_dequeue(item);
+        ccq.wait_dequeue(item);
         i++;
         if (i == 10000000) {
           // sleep(10);
