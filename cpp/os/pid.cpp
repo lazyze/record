386958679591f4c02323ee452558822bbae436c5
lize <GW00301491@ifyou.com>
2023-09-04 10:34:36 +0800

    feat: add pid function demo

---
+++ cpp/os/pid.cpp
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <iostream>
+
+int main() {
+    pid_t childPid = fork();
+
+    if (childPid == 0) {
+        // 子进程执行的代码
+        std::cout << "Child process executing..." << std::endl;
+        exit(123);  // 子进程退出并返回退出状态
+    } else if (childPid > 0) {
+        // 父进程执行的代码
+        int status;
+        pid_t returnedPid = wait(&status);  // 等待子进程终止并获取退出状态
+        if (returnedPid == childPid) {
+            if (WIFEXITED(status)) {
+                std::cout << "Child process exited with status: " << WEXITSTATUS(status) << std::endl;
+            } else {
+                std::cout << "Child process terminated abnormally." << std::endl;
+            }
+        } else {
+            std::cout << "Error waiting for child process." << std::endl;
+        }
+    } else {
+        // fork 失败
+        std::cout << "Failed to create child process." << std::endl;
+    }
+
+    return 0;
+}
+
